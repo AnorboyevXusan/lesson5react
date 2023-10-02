50f0d46a07b2278334dd31914d24af7eb298ebec
AnorboyevXusan <xusananor9@gmail.com>
2023-10-02 11:25:18 +0500

    aaaa

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container text-center">Footer</div>
+    </footer>
+  );
+};
+
+export default Footer;
